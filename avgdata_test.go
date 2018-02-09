bcb27ec06881247dc1a3ecd9fc62488ea704ae69
Tim Waugh <twaugh@redhat.com>
2018-02-09 12:58:11 +0000

    Fix panic-on-failure in test cases
    Signed-off-by: Tim Waugh <twaugh@redhat.com>

--- avgdata_test.go
+++ avgdata_test.go
@@ -6,11 +6,14 @@ func TestNewAvgData(t *testing.T) {
 	avgdata, err := NewAvgData("testdata/simple.csv")
 	if err != nil {
 		t.Error(err)
+		return
 	}
 	if len(avgdata.records) != 1 {
 		t.Errorf("expected 1 record")
+		return
 	}
 	if avgdata.records[0].load != 1 {
 		t.Errorf("unexpected record value")
+		return
 	}
 }
