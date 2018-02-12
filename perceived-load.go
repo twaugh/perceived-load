2d77a2633fdf9e5216af10dabe88438886efc13e
Tim Waugh <twaugh@redhat.com>
2018-02-12 15:45:00 +0000

    Give some useful output
    Signed-off-by: Tim Waugh <twaugh@redhat.com>

--- perceived-load.go
+++ perceived-load.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/jessevdk/go-flags"
 	"fmt"
+	"github.com/jessevdk/go-flags"
+	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,20 +15,38 @@ func main() {
 
 	args, err := flags.ParseArgs(&opts, os.Args[1:])
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	if len(args) > 1 {
-		fmt.Printf("Unexpected parameters: %v\n", args[1:])
-		os.Exit(1)
+		log.Fatalf("Unexpected parameters: %v\n", args[1:])
 	}
 
 	ts := NewTimeSeries()
 	if err := ts.Read(opts.DB); err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println(ts, args, opts.DB)
+	ts.Resample(24 * time.Hour)
+	ts.Interpolate()
+	var today = time.Now().Truncate(24 * time.Hour)
+	days := []int{1, 5, 15}
+	avgs := make([]float64, len(days))
+	var since *TimeSeries
+	for index, days := range days {
+		d := time.Duration((days - 1) * 24) * time.Hour
+		start := today.Add(-d)
+		since = ts.Since(start)
+		for _, record := range since.records {
+			log.Printf("%v: %v\n", record.timestamp.String(), record.datum)
+		}
+		log.Printf("--")
+		var sum float64
+		for _, record := range since.records {
+			sum += record.datum
+		}
+		avgs[index] = sum / float64(len(since.records))
+	}
+
+	fmt.Printf("Perceived task load average (%v days): %v\n", days, avgs)
 }
